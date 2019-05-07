22f886d8f24f4c1c98b4bd058623253c170663e3
forest-icy <xushumaominor@gmail.com>
2019-05-07 17:21:17 +1000

    Create 4 instances and print out the ip addresses to the inventory

--- NeCTAR/run-nectar.sh
+++ NeCTAR/run-nectar.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-. ./openrc.sh; ansible-playbook --ask-become-pass nectar.yaml
+# create webserver instance
+. ./openrc.sh; ansible-playbook --ask-become-pass web_server.yaml
+
+echo "The webserver instance is created"
+
+# create dbserver instance
+. ./openrc.sh; ansible-playbook --ask-become-pass db_server.yaml
+
+echo "The dbserver instance is created"
+
+# create harvester1 instance
+. ./openrc.sh; ansible-playbook --ask-become-pass harvester1.yaml
+
+echo "The harvester1 instance is created"
+
+# create harvester2 instance
+. ./openrc.sh; ansible-playbook --ask-become-pass harvester2.yaml
+
+echo "The harvester2 instance is created"
