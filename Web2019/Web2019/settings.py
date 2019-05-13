7f0112121273831d6a70c48f33a15a37f1340a28
Dinghao <vicydh@gmail.com>
2019-05-14 05:28:53 +1000

    upload the web application (backend and frontend)

--- Web2019/Web2019/settings.py
+++ Web2019/Web2019/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x=0py+r93ni@t5q42w6q_7f#p^j2rjb0h%3of^jx+%mpom97hb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -122,3 +122,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
