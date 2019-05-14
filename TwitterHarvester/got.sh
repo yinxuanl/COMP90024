6b5eb114e3bcaf3252dae8d8aa25b22e3dd584e5
yinxuan li <krysxuanxuan@gmail.com>
2019-05-15 09:35:36 +1000

    Update search key words

--- TwitterHarvester/got.sh
+++ TwitterHarvester/got.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python3 TwitterSearch.py -q '#gameofhrones' -d 'got_tweets' -t 'got_user_timelines' -u 'got_users' &
+python3 TwitterSearch.py -q 'game of thrones' -d 'got_tweets' -t 'got_user_timelines' -u 'got_users' &
